1dc91ac0a04d90dee618deba12d17c726d33b7c9
iswudi <iswudi@buaa.edu.cn>
2020-04-07 18:24:06 +0800

    complete lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A.rest == null){
+            A.rest = B;
+        }else{
+            A.rest = dcatenate(A.rest,B);
+        }
+        return A;
     }
 
     /**
@@ -91,7 +96,11 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A.rest == null){
+            return new IntList(A.first,B);
+        }else{
+            return new IntList(A.first,catenate(A.rest,B));
+        }
     }
 
 
