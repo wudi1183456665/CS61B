0fbec92237516a5c0f7f2f5a77a49fa6da1dc90e
iswudi <iswudi@buaa.edu.cn>
2020-04-07 15:47:48 +0800

    complete proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,21 +1,21 @@
 public class ArrayDeque <T> {
     /* Use a circular Array. */
 
-    public T[] items;
+    private T[] items;
     private int size;
-    public int nextFirst;
-    public int nextLast;
+    private int nextFirst;
+    private int nextLast;
 
     private static double RFACTOR = 0.25;
 
-    public ArrayDeque (){
+    public ArrayDeque () {
         items = (T[]) new Object[8];
         nextFirst = 4;
         nextLast = 5;
         size = 0;
     }
 
-    public ArrayDeque (ArrayDeque other){
+    public ArrayDeque (ArrayDeque other) {
         items = (T[]) new Object[other.items.length];
         nextFirst = other.nextFirst;
         nextLast = other.nextLast;
@@ -23,7 +23,7 @@ public class ArrayDeque <T> {
         System.arraycopy(other.items,0,items,0,other.items.length);
     }
 
-    private void resize(int capacity){
+    private void resize(int capacity) {
         T[] newItems = (T[]) new Object[capacity];
         System.arraycopy(items,nextFirst + 1,newItems, capacity / 2 + 1,items.length - 1 - nextFirst);
         System.arraycopy(items,0,newItems,capacity / 2 + items.length - nextFirst,nextLast);
@@ -34,7 +34,7 @@ public class ArrayDeque <T> {
 
     public void addFirst(T item){
         if (nextLast == nextFirst){
-            resize(items.length * (int)(1/RFACTOR));
+            resize(items.length * (int) (1 / RFACTOR));
         }
         items[nextFirst] = item;
         size += 1;
@@ -43,23 +43,23 @@ public class ArrayDeque <T> {
 
     public void addLast(T item){
         if (nextLast == nextFirst){
-            resize(items.length * (int)(1/RFACTOR));
+            resize(items.length * (int)(1 / RFACTOR));
         }
         items[nextLast] = item;
         size += 1;
         nextLast = (nextLast + 1) % items.length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
-        for( int i = nextFirst + 1;i <= nextFirst + size; i++){
+    public void printDeque() {
+        for( int i = nextFirst + 1; i <= nextFirst + size; i++){
             T value = items[i % items.length];
             System.out.print(value);
             if (i == nextFirst + size){
@@ -74,15 +74,15 @@ public class ArrayDeque <T> {
         T[] newItems = (T[]) new Object[capacity];
         if (nextFirst + size > items.length - 1){
             // now array is a circular
-            System.arraycopy(items,nextFirst + 1,newItems,1,items.length - 1 - nextFirst);
-            System.arraycopy(items,0,newItems,items.length - nextFirst,nextLast);
+            System.arraycopy(items, nextFirst + 1, newItems, 1, items.length - 1 - nextFirst);
+            System.arraycopy(items, 0, newItems, items.length - nextFirst, nextLast);
             nextFirst = 0;
             nextLast = (1 + size) % capacity;
             items = newItems;
 
         }else{
             // now array is not a circular
-            System.arraycopy(items,nextFirst + 1,newItems,1,size);
+            System.arraycopy(items, nextFirst + 1, newItems, 1,size);
             nextFirst = 0;
             nextLast = (1 + size) % capacity;
             items = newItems;
@@ -94,17 +94,17 @@ public class ArrayDeque <T> {
         items[nextFirst + 1] = null;
         nextFirst = (nextFirst + 1) % items.length;
         size -= 1;
-        if ((double)size/items.length < RFACTOR){
-            downSize((int)(items.length * RFACTOR));
+        if ((double) size / items.length < RFACTOR){
+            downSize((int) (items.length * RFACTOR));
         }
         return firstValue;
 
     }
 
-    public T removeLast(){
+    public T removeLast() {
         T lastValue = items[nextLast - 1];
         if ((double)size/items.length < RFACTOR){
-            downSize(size * (int)(1/RFACTOR));
+            downSize(size * (int) (1 / RFACTOR));
         }
         items[nextLast - 1] =null;
         nextLast = (items.length + nextLast - 1) % items.length;
@@ -112,7 +112,7 @@ public class ArrayDeque <T> {
         return lastValue;
     }
 
-    public T get(int index){
+    public T get(int index) {
         return items[(nextFirst + 1 + index) % items.length];
     }
 }
